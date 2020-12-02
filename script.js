452dfc46e1a5ca8579ef2ec2f147a368b22f7bc6
OOPArt4922 <danicandido02@gmail.com>
2020-12-02 17:12:06 +0100

    now prettier and with user input

--- script.js
+++ script.js
@@ -1,75 +1,187 @@
-const functions = [
-	{ F: "x^2", f: () => `sqrt(x - 1/${$("#plotSlider").val() - 1})^4`, title: "x<sup>2</sup>" },
-	{ F: "x^3", f: () => `nthRoot(x - 1/${$("#plotSlider").val() - 1}, 3)^9`, title: "x<sup>3</sup>" },
-	{ F: "sqrt(x)", f: () => `sqrt(x - 1/${$("#plotSlider").val() - 1})`, title: "<span>&#8730;</span>x<sup></sup>" },
-	{ F: "x", f: () => `x - 1/${$("#plotSlider").val() - 1}`, title: "x<sup></sup>" },
-];
+//#region Loading LocalStorage Data
 
-const f = x => functionPlot.$eval.builtIn(plot.options.data[0], "fn", { x: x });
+const funcLatex = localStorage.getItem("funcLatex") === "" ? "x^2" : localStorage.getItem("funcLatex") ?? "x^2";
+$("#plotSlider")
+	.val(JSON.parse(localStorage.getItem("sliderVal")) ?? 2)
+	.change();
+$("#plotSlider").on("input", changeSlider);
+$("#sliderLabel").text(`Numero di suddivisioni: ${$("#plotSlider").val()}`);
 
-let IDX = JSON.parse(localStorage.getItem("functionIdx") ?? 0);
-$("#plotSlider").val(JSON.parse(localStorage.getItem("sliderVal") ?? 14)).change();
+let [a, b] = [JSON.parse(localStorage.getItem("rangeA")) ?? 0, JSON.parse(localStorage.getItem("rangeB")) ?? 1];
+$("#range span:nth-child(1)").text(a);
+$("#range span:nth-child(2)").text(b);
 
-let plot = functionPlot({
-	target: "#plot",
-	disableZoom: true,
-	width: getSize()[0],
-	height: getSize()[1],
-	grid: true,
-	xAxis: { domain: [0, 1.25] },
-	yAxis: { domain: [0, 1.25] },
-	data: [
-		{
-			fn: functions[IDX].F,
-			color: "#0000ff",
-		},
-		{
-			fn: functions[IDX].F,
-			color: "#ff0000",
-			range: [0, 1],
-			nSamples: $("#plotSlider").val(),
-			closed: true,
-		},
-		{
-			fn: functions[IDX].f(),
-			color: "#00ff00",
-			range: [0, 1],
-			nSamples: $("#plotSlider").val(),
-			closed: true,
-			skipTip: true,
-		},
-	],
-});
-plot.meta.yAxis.tickFormat(d => "" + +d);
-plot.meta.xAxis.tickFormat(d => "" + +d);
-plot.draw();
+//#endregion Loading LocalStorage Data
 
-$("#title").html("f(x) = " + functions[IDX].title);
+//#region Plot Expressions Loading
 
-$(window).resize(() => {
-	[plot.options.width, plot.options.height] = getSize();
-
-	$("#plot").html("");
-	plot = plot.build();
-
-	plot.meta.yAxis.tickFormat(d => "" + +d);
-	plot.meta.xAxis.tickFormat(d => "" + +d);
-	plot.draw();
+const plot = Desmos.GraphingCalculator(document.querySelector("#plot"), {
+	keypad: false,
+	expressions: false,
+	settingsMenu: false,
 });
 
-$("#titleContainer .button").click(e => {
-	if ($(e.currentTarget).children().hasClass("right")) {
-		if (IDX < functions.length - 1) changeFunction(++IDX);
-		else changeFunction((IDX = 0));
-	} else {
-		if (IDX > 0) changeFunction(--IDX);
-		else changeFunction((IDX = 3));
+plot.setState({
+	expressions: {
+		list: [
+			{ type: "expression", id: "function", latex: `f(x)=${funcLatex}`, color: Desmos.Colors.BLUE },
+			{ type: "expression", id: "subdivisions", latex: `n=${$("#plotSlider").val()}` },
+			{ type: "expression", id: "sub_list", latex: "i=[1...n]" },
+			{
+				type: "expression",
+				id: "slider_A",
+				latex: `a=${a}`,
+				slider: { hardmax: true, hardmin: true, min: -1000, max: 1000, step: 0.1 },
+			},
+			{ type: "expression", id: "vertical_A", latex: "x=a", color: Desmos.Colors.GREEN },
+			{ type: "expression", id: "point_A", latex: "(a, 0)", color: Desmos.Colors.GREEN, label: "a", showLabel: true },
+			{
+				type: "expression",
+				id: "slider_B",
+				latex: `b=${b}`,
+				slider: { hardmax: true, hardmin: true, min: -1000, max: 1000, step: 0.1 },
+			},
+			{ type: "expression", id: "vertical_B", latex: "x=b", color: Desmos.Colors.GREEN },
+			{ type: "expression", id: "point_B", latex: "(b, 0)", color: Desmos.Colors.GREEN, label: "b", showLabel: true },
+			{
+				type: "expression",
+				id: "positive_area",
+				latex: "0\\le y\\le f\\left(x\\right)\\left\\{\\left(a-x\\right)\\left(b-x\\right)<0\\right\\}",
+				color: Desmos.Colors.RED,
+			},
+			{
+				type: "expression",
+				id: "negative_area",
+				latex: "f\\left(x\\right)\\le y\\le0\\left\\{\\left(a-x\\right)\\left(b-x\\right)<0\\right\\}",
+				color: Desmos.Colors.BLUE,
+			},
+			{
+				type: "expression",
+				id: "sum_list",
+				latex:
+					"S=\\left[\\sum_{j=1}^{n}f\\left(a+\\left(j-1\\right)\\left(\\frac{b-a}{n}\\right)\\right)\\left(\\frac{b-a}{n}\\right),\\sum_{j=1}^{n}f\\left(a+j\\left(\\frac{b-a}{n}\\right)\\right)\\left(\\frac{b-a}{n}\\right),\\int_{a}^{b}f\\left(x\\right)dx\\right]",
+				hidden: true,
+			},
+			{ id: "rect_folder", title: "rect_folder", type: "folder" },
+			{
+				type: "expression",
+				id: "big_rect",
+				latex:
+					"\\min\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\left\\{\\min\\left(b,a\\right)+\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left(i-1\\right)\\le x\\le\\min\\left(b,a\\right)+\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}i\\right\\}",
+				color: Desmos.Colors.RED,
+				folderId: "rect_folder",
+			},
+			{
+				type: "expression",
+				id: "big_rect_vert1",
+				latex:
+					"x=\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left\\{\\min\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\right\\}",
+				color: Desmos.Colors.RED,
+				folderId: "rect_folder",
+			},
+			{
+				type: "expression",
+				id: "big_rect_vert2",
+				latex:
+					"x=\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left\\{\\min\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\right\\}",
+				color: Desmos.Colors.RED,
+				folderId: "rect_folder",
+			},
+			{
+				type: "expression",
+				id: "small_rect",
+				latex:
+					"\\min\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\left\\{\\min\\left(b,a\\right)+\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left(i-1\\right)\\le x\\le\\min\\left(b,a\\right)+\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}i\\right\\}",
+				color: Desmos.Colors.GREEN,
+				folderId: "rect_folder",
+			},
+			{
+				type: "expression",
+				id: "small_rect_vert1",
+				latex:
+					"x=\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left\\{\\min\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\right\\}",
+				color: Desmos.Colors.GREEN,
+				folderId: "rect_folder",
+			},
+			{
+				type: "expression",
+				id: "small_rect_vert2",
+				latex:
+					"x=\\min\\left(b,a\\right)+i\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\left\\{\\min\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\le y\\le\\max\\left(0,f\\left(\\min\\left(b,a\\right)+\\left(i-1\\right)\\frac{\\max\\left(b,a\\right)-\\min\\left(b,a\\right)}{n}\\right)\\right)\\right\\}",
+				color: Desmos.Colors.GREEN,
+				folderId: "rect_folder",
+			},
+		],
+	},
+	graph: {
+		viewport: {
+			xmin: -2,
+			xmax: 2,
+			ymin: -1,
+			ymax: 2,
+		},
+	},
+	version: 7,
+	randomSeed: "",
+});
+plot.newRandomSeed();
+
+//#endregion Plot Expressions Loading
+
+//#region Helper Expressions
+
+const hAB = plot.HelperExpression({ latex: "[a, b]" });
+hAB.observe("listValue", () => {
+	a = hAB.listValue[0];
+	b = hAB.listValue[1];
+	$("#range span:nth-child(1)").text(a);
+	$("#range span:nth-child(2)").text(b);
+	localStorage.setItem("rangeA", a);
+	localStorage.setItem("rangeB", b);
+});
+
+const hS = plot.HelperExpression({ latex: "S" });
+hS.observe("listValue", () => {
+	if (hS.listValue) {
+		let [s, S, I] = !isNaN(hS.listValue[2])
+			? [hS.listValue[0].toFixed(10), hS.listValue[1].toFixed(10), hS.listValue[2].toFixed(10)]
+			: ["Intervallo non valido", "Intervallo non valido", "Intervallo non valido"];
+
+		if (I == 0) [s, S, I] = [0, 0, 0];
+
+		$("#result").html(
+			`<span style="color: #60b030">S<sub>n</sub>: ${s}</span><br>
+			<span style="color: #ff5060">S<sub>N</sub>: ${S}</span><br>
+			<span style="color: #6050ff">Valore Reale: ${I}</span><br>`
+		);
+
+		plot.setExpression({
+			id: "rect_folder",
+			hidden: isNaN(hS.listValue[2]),
+		});
 	}
-	localStorage.setItem("functionIdx", "" + IDX);
 });
+
+//#endregion Helper Expressions
+
+//#region Mathquill Editor Settings
+
+const answerMathField = MathQuill.getInterface(2).MathField(document.querySelector("#latex"), {
+	handlers: {
+		edit: () => {
+			plot.setExpression({ id: "function", latex: `f(x)=${answerMathField.latex()}` });
+			localStorage.setItem("funcLatex", answerMathField.latex());
+		},
+	},
+});
+answerMathField.latex(funcLatex);
+
+//#endregion Mathquill Editor Settings
+
+//#region Arrow Buttons Code
 
 let timeout, interval;
-$("#input .button").on("mousedown", e => {
+$("#slider .button").on("mousedown", e => {
 	sliderButtons(e);
 	changeSlider();
 
@@ -85,44 +197,15 @@ $(".button").on("mouseup mouseleave", () => {
 	clearInterval(interval);
 });
 
-$("#plotSlider").on("input", () => changeSlider());
+//#endregion Arrow Buttons Code
 
-$("#sliderLabel").text(`Numero di suddivisioni: ${$("#plotSlider").val() - 1}`);
-calculate($("#plotSlider").val() - 1, "#result");
-
-function calculate(n, label) {
-	let [s, S] = [0, 0];
-	let base = 1 / n;
-
-	for (let i = 0; i < n; i++) {
-		s += f(i * base) * base;
-		S += f((i + 1) * base) * base;
-	}
-
-	$(label).html(
-		`<span style="color: #60b030">S<sub>n</sub>: ${s.toFixed(10)}</span><br>
-        <span style="color: #ff5060">S<sub>N</sub>: ${S.toFixed(10)}</span><br>
-        Precisione: ${isNaN(((s / S) * 100).toFixed(3)) ? 0 : ((s / S) * 100).toFixed(3)}%`
-	);
-}
+//#region Helper functions
 
 function changeSlider() {
-	localStorage.setItem("sliderVal", $("#plotSlider").val());
-	$("#sliderLabel").text(`Numero di suddivisioni: ${$("#plotSlider").val() - 1}`);
-	plot.options.data[2].nSamples = $("#plotSlider").val();
-	plot.options.data[1].nSamples = $("#plotSlider").val();
-	plot.options.data[2].fn = functions[IDX].f();
-	plot.draw();
-	calculate($("#plotSlider").val() - 1, "#result");
-}
-
-function changeFunction(x) {
-	plot.options.data[0].fn = functions[x].F;
-	plot.options.data[1].fn = functions[x].F;
-	plot.options.data[2].fn = functions[x].f();
-	$("#title").html("f(x) = " + functions[IDX].title);
-	calculate($("#plotSlider").val() - 1, "#result");
-	plot.draw();
+	let v = $("#plotSlider").val();
+	localStorage.setItem("sliderVal", v);
+	plot.setExpression({ id: "subdivisions", latex: `n=${v}` });
+	$("#sliderLabel").text(`Numero di suddivisioni: ${v}`);
 }
 
 function sliderButtons(e) {
@@ -131,15 +214,4 @@ function sliderButtons(e) {
 		.change();
 }
 
-function getSize() {
-	let width = 800;
-	let height = document.body.getBoundingClientRect().height;
-	let ratio = document.body.getBoundingClientRect().width / width;
-	width *= ratio;
-	if (innerWidth > 1000) {
-		width *= 60 / 100;
-		height *= 9 / 10;
-	} else height *= 2 / 3;
-
-	return [width, height];
-}
+//#endregion Helper functions
